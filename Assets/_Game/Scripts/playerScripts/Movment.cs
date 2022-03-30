cfa2b832a980eb7927f16bc1e2a66cf3f86dab3c
DanielTzafrir <74878286+DanielTzafrir@users.noreply.github.com>
2022-03-30 08:21:01 +0300

    damge animation
    and a few more things

---
+++ Assets/_Game/Scripts/playerScripts/Movment.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator ani;
+    private float horizontalMove;
+    private bool isGrounded;
+    private bool isCeiling;
+    private bool isInfrontAWall;
+    private float stopRun = 0;
+
+    public Transform groundCheck;
+    public Transform ceilingCheck;
+    public Transform WallCheck;
+    public Transform WallCheck2;
+    public float speed = 0;
+    public float jumpSpeed = 10;
+    public float slideSpeed = 10;
+    public List<GameObject> traps;
+    public LayerMask grouneLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        MovementPlayer();
+        ani.SetTrigger("onTrap");
+    }
+
+    private void MovementPlayer()
+    {
+        isInfrontAWall = Physics2D.OverlapCircle(WallCheck.position, 0.2f, grouneLayer) || Physics2D.OverlapCircle(WallCheck2.position, 0.2f, grouneLayer);
+        if (isInfrontAWall)
+        {
+            horizontalMove = stopRun;
+        }
+        else
+        {
+            horizontalMove = speed;
+        }
+        
+    }
+
+    public void jumpButton()
+    {
+        isGrounded = Physics2D.OverlapCircle( groundCheck.position, 0.2f, grouneLayer );
+        isCeiling = Physics2D.OverlapCircle( ceilingCheck.position, 0.2f, grouneLayer );
+        
+        if (isGrounded && !isCeiling)
+        {
+            ani.SetTrigger("jump");
+            rb.velocity = Vector2.up * jumpSpeed;
+           
+        }
+    }
+
+    public void holdingSlideButton()
+    {
+        ani.SetTrigger("press_slide");
+        rb.velocity = Vector2.down * slideSpeed;
+    }
+    
+    public void notHoldingSlideButton()
+    {
+        ani.SetTrigger("no_press_slide");
+        rb.velocity = Vector2.down * slideSpeed;
+    }
+    public void slideButton()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, grouneLayer);
+
+        if (isGrounded)
+        {
+            ani.SetTrigger("slide");
+            rb.velocity = Vector2.down * slideSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D (Collider2D collider2D)
+    {
+        Debug.Log("4");
+        if (traps.Contains(collider2D.gameObject))
+        {
+            Debug.Log("1");
+            takeDamage();
+        }    
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //For the health bar
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (traps.Contains(collision.gameObject))
+        {
+            Debug.Log("3");
+            ani.SetTrigger("onTrap");
+        }
+    }
+    public void takeDamage()
+    {
+        Debug.Log("2");
+        ani.SetTrigger("onTrap");
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
+    }
+}
