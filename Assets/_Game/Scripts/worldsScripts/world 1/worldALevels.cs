3b25bcb1a8959cad4121e8f53ddacdeb8cfa0b50
DanielTzafrir <74878286+DanielTzafrir@users.noreply.github.com>
2022-04-10 10:48:18 +0300

    lvl 1 finished

--- Assets/_Game/Scripts/worldsScripts/world 1/worldALevels.cs
+++ Assets/_Game/Scripts/worldsScripts/world 1/worldALevels.cs
@@ -12,6 +12,28 @@ public class worldALevels : MonoBehaviour
 
     public void LoadLvl1()
     {
-        SceneManager.LoadScene("lvls world 1");
+        SceneManager.LoadScene("lvl 1");
     }
+    public void LoadLvl2()
+    {
+        //SceneManager.LoadScene("lvl 2");
+        Debug.Log("1");
+    }
+    public void LoadLvl3()
+    {
+        //SceneManager.LoadScene("lvl 3");
+    }
+    public void LoadLvl4()
+    {
+        //SceneManager.LoadScene("lvl 4");
+    }
+    public void LoadLvl5()
+    {
+        //SceneManager.LoadScene("lvl 5");
+    }
+    public void LoadLvl6()
+    {
+        //SceneManager.LoadScene("lvl 6");
+    }
+
 }
