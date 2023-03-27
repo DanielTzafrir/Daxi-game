3945d0741374e8d6de101b69ef8a394ea3dbb476
nadavzeldin <52635084+nadavzeldin@users.noreply.github.com>
2023-03-27 16:44:28 +0300

    add damage and collsion

--- Assets/_Game/Scripts/playerScripts/PlayerMovement.cs
+++ Assets/_Game/Scripts/playerScripts/PlayerMovement.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float moveSpeed = 5f; // Speed of player movement
+    [SerializeField] private float baseMoveSpeed = 5f;
+    [SerializeField] private float moveSpeed; // Speed of player movement
     [SerializeField] private float jumpSpeed = 10f;
     [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private Animator animator;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask Ground;
-    [SerializeField] private CapsuleCollider2D bodyCollider;   
+    [SerializeField] private CapsuleCollider2D bodyCollider;
+    [SerializeField] private GameObject shield;
 
     private void Start()
     {
+        moveSpeed = baseMoveSpeed;
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 }
@@ -23,6 +26,10 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rigidBody.velocity = new Vector2(moveSpeed, rigidBody.velocity.y);
+        if(moveSpeed > baseMoveSpeed)
+        {
+            moveSpeed = baseMoveSpeed + moveSpeed / 2;
+        }
     }
     public void JumpEvent()
     {
@@ -42,13 +49,12 @@ public class PlayerMovement : MonoBehaviour
     public void SlideEvent()
     {
         Debug.Log("sLIDE event");
-
+        animator.SetTrigger("start_sliding");
         Slide();
     }
 
     private void Slide()
     {        
-        animator.SetTrigger("start_sliding");
         bodyCollider.enabled = true;
         rigidBody.velocity = Vector2.down * 3f;
         animator.SetTrigger("press_slide");
@@ -61,29 +67,48 @@ public class PlayerMovement : MonoBehaviour
         animator.SetTrigger("no_press_slide");
         bodyCollider.enabled = false;
     }
-    /*
-    public void holdingSlideButton()
+    
+    public void HoldingSlideButton()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, grouneLayer);
-
-        if (isGrounded && !gum)
-        {
-            ani.SetTrigger("start_sliding");
-            ani.SetTrigger("press_slide");
-            rb.velocity = Vector2.down * slideSpeed;
-        }
-
+        bodyCollider.enabled = true;
+        rigidBody.velocity = Vector2.down * 3f;
+        animator.SetTrigger("press_slide");
     }
 
-    public void notHoldingSlideButton()
+    public void NotHoldingSlideButton()
     {
-        if (!gum)
+        StopSliding();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        switch(collider2D.tag)
         {
-            ani.SetTrigger("no_press_slide");
-            ani.ResetTrigger("start_sliding");
-            ani.ResetTrigger("press_slide");
-            StartCoroutine(stoptrigger());
-            rb.velocity = Vector2.down * slideSpeed;
-        }
-    }*/
+            case "Enemy":
+                {
+                    if (shield.activeSelf)
+                        return;
+                    TakeDamage();
+                    break;
+                }
+            case "speedBoost":
+                {
+                    moveSpeed *= 1.6f;
+                    break;
+                }
+            case "element":
+                {
+                    break;
+                }
+            default:
+                // code block
+                break;
+        }      
+    }
+
+    public void TakeDamage()
+    {
+        Debug.Log("Damage");
+        animator.SetTrigger("onTrap");               
+    }
 }
